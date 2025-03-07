835037d2db5c25638841bb87a638e432b00f102e
thisaramax20 <thisarathejana20@gmail.com>
2025-03-07 12:14:06 +0530

    create logger in identity service

---
+++ identity-service/src/models/User.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const argon2 = require("argon2");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      minlength: 3,
+      maxlength: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//add extra functionality
+userSchema.pre("save", async (next) => {
+  if (this.isModified(password)) {
+    try {
+      this.password = await argon2.hash(this.password);
+    } catch (error) {
+      console.error("Error hashing password:", error);
+      next(error);
+    }
+  }
+});
+
+userSchema.method.comparePassword = async (candidatePasswords) => {
+  try {
+    return await argon2.verify(this.password, candidatePasswords);
+  } catch (error) {}
+};
+
+userSchema.index({ ussername: "text" });
+
+module.exports = mongoose.model("User", userSchema);
